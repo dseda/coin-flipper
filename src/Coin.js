3e9a98964c7a79a7f0d9338fec147ef5d3717d72
sed <sedademirgf@gmail.com>
2021-10-29 08:54:46 +0100

    flip coin

--- src/Coin.js
+++ src/Coin.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import heads from "./heads.png";
-import tails from "./tails.png";
 import "./Coin.css";
+
 class Coin extends Component {
-  static defaultProps = {
-    source: tails,
-  };
   render() {
-    return <img className="Coin" src={this.props.source} />;
+    return (
+      <div>
+        <img className="Coin" src={this.props.source} />
+      </div>
+    );
   }
 }
 export default Coin;
--- src/Flipper.js
+++ src/Flipper.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import "./Flipper.css";
 import Coin from "./Coin";
-
+import heads from "./heads.png";
+import tails from "./tails.png";
 class Flipper extends Component {
+  static defaultProps = {
+    faces: [tails, heads],
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentFace: this.flip(),
+    };
+  }
+
+  flip() {
+    return this.props.faces[Math.floor(Math.random() * 2)];
+  }
   render() {
     return (
       <div>
-        <Coin />
+        <Coin source={this.state.currentFace} />
       </div>
     );
   }
