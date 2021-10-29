459fac31f83a3f96691b377892bc38b2f2d1bf24
sed <sedademirgf@gmail.com>
2021-10-29 10:00:37 +0100

    count heads and tails

--- src/Flipper.js
+++ src/Flipper.js
@@ -12,21 +12,32 @@ class Flipper extends Component {
     this.state = {
       currentFace: tails,
       isFlipping: false,
+      flipCount: 0,
+      headsCount: 0,
+      tailsCount: 0,
     };
     this.flip = this.flip.bind(this);
   }
 
   flip(e) {
-    this.setState({
-      currentFace: this.props.faces[Math.floor(Math.random() * 2)],
-      isFlipping: true,
+    this.setState((st) => {
+      return {
+        currentFace: this.props.faces[Math.floor(Math.random() * 2)],
+        isFlipping: true,
+        //How to update a state. st is the current state object
+        flipCount: st.flipCount + 1,
+        headsCount:
+          st.currentFace === heads ? st.headsCount + 1 : st.headsCount,
+        tailsCount:
+          st.currentFace === tails ? st.tailsCount + 1 : st.tailsCount,
+      };
     });
     setTimeout(
       () =>
         this.setState({
           isFlipping: false,
         }),
-      700
+      1000
     );
   }
   render() {
@@ -36,8 +47,12 @@ class Flipper extends Component {
           flipping={this.state.isFlipping}
           source={this.state.currentFace}
         />
+        <p>Total flips: {this.state.flipCount}</p>
+        <p>Total heads: {this.state.headsCount}</p>
+        <p>Total tails: {this.state.tailsCount}</p>
         <button
           onClick={this.flip}
+          disabled={this.state.isFlipping}
           className={this.state.isFlipping ? "flipping" : ""}
         >
           {this.state.isFlipping ? "Flipping..." : "Flip me!"}
